2da7f08447b0ef6af0bbdb7d46027f5d02ad7cf0
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-08-21 21:01:32 +0100

    Use keyset::Drawing internally in Drawing

--- src/core/drawing.rs
+++ src/core/drawing.rs
@@ -10,12 +10,9 @@ pub fn module<'p>(py: Python<'p>) -> PyResult<&'p PyModule> {
     Ok(drawing)
 }
 
-#[pyclass(module = "pykeyset._impl.core.layout")]
+#[pyclass(module = "pykeyset._impl.core.drawing")]
 #[derive(Debug, Clone)]
-pub struct Drawing {
-    keys: Vec<keyset::Key>,
-    opts: keyset::DrawingOptions,
-}
+pub struct Drawing(keyset::Drawing);
 
 #[pymethods]
 impl Drawing {
@@ -23,25 +20,22 @@ impl Drawing {
     fn new(layout: Layout, profile: Profile, font: Font) -> PyResult<Self> {
         let mut options = keyset::DrawingOptions::default();
         options.profile(profile.0).font(font.0);
-        Ok(Self {
-            keys: layout.0,
-            opts: options,
-        })
+        Ok(Self(options.draw(&layout.0)))
     }
 
     fn to_svg(&self) -> PyResult<String> {
-        Ok(self.opts.draw(&self.keys).to_svg())
+        Ok(self.0.to_svg())
     }
 
     fn to_png(&self) -> PyResult<Vec<u8>> {
-        Ok(self.opts.draw(&self.keys).to_png(96.))
+        Ok(self.0.to_png(96.))
     }
 
     fn to_pdf(&self) -> PyResult<Vec<u8>> {
-        Ok(self.opts.draw(&self.keys).to_pdf())
+        Ok(self.0.to_pdf())
     }
 
     fn to_ai(&self) -> PyResult<Vec<u8>> {
-        Ok(self.opts.draw(&self.keys).to_ai())
+        Ok(self.0.to_ai())
     }
 }
