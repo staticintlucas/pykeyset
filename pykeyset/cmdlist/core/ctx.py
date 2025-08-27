fa98d5926f8e7bc97be5537d21a4dec94792a4e2
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2025-08-27 21:49:19 +0100

    Remove python components

--- pykeyset/cmdlist/core/ctx.py
+++
@@ -1,8 +0,0 @@
-class Context:
-    def __init__(self, name):
-        self.name = name
-        self.font = None
-        self.icons = []
-        self.profile = None
-        self.layout = None
-        self.drawing = None
