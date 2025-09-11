96b9e4bd5dfa6cd74d366ec76ad8b3ad98d4429f
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2025-09-11 19:01:15 +0100

    Use declarative module

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,25 @@ mod core;
 mod version;
 
 #[pymodule]
-fn pykeyset(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add("version_info", VERSION)?;
-    m.add("version", VERSION_STR)?;
-    m.add("__version__", VERSION_STR)?;
-    m.add_function(wrap_pyfunction!(version::build_info, m)?)?;
+mod pykeyset {
+    use super::*;
 
-    m.add_class::<core::Font>()?;
-    // m.add_class::<core::icon::Icon>()?;
-    // m.add_class::<core::icon::IconSet>()?;
-    m.add_class::<core::Layout>()?;
-    m.add_class::<core::Profile>()?;
-    m.add_class::<core::Drawing>()?;
-    // m.add_class::<core::drawing::DrawingOptions>()?;
+    #[pymodule_export]
+    use super::core::Font;
+    #[pymodule_export]
+    use super::core::Layout;
+    #[pymodule_export]
+    use super::core::Profile;
+    #[pymodule_export]
+    use super::core::Drawing;
 
-    Ok(())
+    #[pymodule_init]
+    fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
+        // TODO can't add variables to declarative modules
+        m.add("version_info", VERSION)?;
+        m.add("version", VERSION_STR)?;
+        m.add("__version__", VERSION_STR)?;
+        m.add_function(wrap_pyfunction!(version::build_info, m)?)?;
+        Ok(())
+    }
 }
